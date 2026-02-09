449230956fea2e3b970c30c433e7955f48346a8b
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-09 11:31:53 +0530

    Update state.js

---
+++ js/core/state.js
@@ -0,0 +1,14 @@
+export const AppState = {
+  rawData: {
+    sales: [],
+    stock: [],
+    styleStatus: [],
+    saleDays: [],
+    sizeCount: [],
+    production: []
+  },
+  loadProgress: {
+    total: 6,
+    completed: 0
+  }
+};
