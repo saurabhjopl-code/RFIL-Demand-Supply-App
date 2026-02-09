52331c0c73812b6fa205c07e5950202595f615ba
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-09 11:51:13 +0530

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -7,6 +7,19 @@ export const AppState = {
     sizeCount: [],
     production: []
   },
+
+  filters: {
+    month: "Latest Month",
+    fc: "All FC",
+    category: "All Categories",
+    remark: "All Company Remarks",
+    search: ""
+  },
+
+  filteredData: {
+    sales: []
+  },
+
   loadProgress: {
     total: 6,
     completed: 0
